5cba3be4e1c2ac2f757f7d7294da7b5a23d7c556
H. Mokhtarzadeh <mokh0006@umn.edu>
2024-01-31 17:24:49 -0600

    Fit CL data using MATLAB script

--- curve_fitting/curve_fit_CL.m
+++ curve_fitting/curve_fit_CL.m
@@ -8,9 +8,28 @@ CL = data(:, 2);
 %% Visualize data
 figure;
 plot(aoa_deg, CL);
-xlabel('Angle of Attack (deg)')
-ylabel('C_L')
+xlabel('Angle of Attack (deg)');
+ylabel('C_L');
+hold on
 
 %% Define and fit a model
+% Trim to linear region of data.
+m = aoa_deg < 22;
+x = aoa_deg(m);
+y = CL(m);
+
+% Fit the model.
+p = polyfit(x, y, 1);
+disp('MATLAB Poly. Coefficients:')
+disp(p);
+
+% Estimate CL using model
+y_fit = polyval(p, x);
+% y_fit = p(1)*x + p(2)
+
 
 %% Visualize the result
+
+plot(x, y,'*k');
+plot(x, y_fit, '-m');
+
