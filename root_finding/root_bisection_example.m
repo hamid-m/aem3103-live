3b289227d01ce986beed513a779154dcf96bdfa2
H. Mokhtarzadeh <mokh0006@umn.edu>
2024-03-18 16:38:29 -0500

    Added stopping conditions to bisection example

--- root_finding/root_bisection_example.m
+++ root_finding/root_bisection_example.m
@@ -7,6 +7,8 @@ g = 32.2; % ft/s^2
 f = @(theta_deg) x * tand(theta_deg) ...
     - 0.5*x^2*g/v0^2./cosd(theta_deg).^2 ...
     + hQ - 7;
+imax = 10;
+tol = 1; % tolerance in solution (deg)
 
 % Visualize & Select an Interval
 thetas_deg = 1:70;
@@ -21,8 +23,9 @@ a = 20; b = 40;
 plot([a, b], [0, 0], 'LineWidth', 2);
 
 % Bisection Algorithm
-for i = 1:10
+for i = 1:imax
     xns = (a + b)/2;
+    toli = abs(b-a)/2;
     
     % Two candidate brackets:
     % [a xns] or [xns b]
@@ -32,6 +35,16 @@ for i = 1:10
     fxns = f(xns);
     
     fprintf("%d  %f  %f  %f  %f\n", i, a, b, xns, f(xns));
+
+    % Decide on stopping
+    if i == imax
+        exit_msg = 'Max iterations reached.'
+        break
+    end
+    if toli < tol
+        exit_msg = 'Tolerance satisfied.'
+        break
+    end
     
     if fa*fxns < 0
         b = xns;
@@ -51,6 +64,7 @@ end
 
 
 % View Result
+disp(exit_msg);
 fprintf("theta: %f\n", xns);
 plot(xns, f(xns), 'k*');
 
